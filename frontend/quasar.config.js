5aa48b3af7f3f2b50d33dbdffcbf81399f00fbff
high-horse <akhileshadhikaria1@gmail.com>
2025-05-14 22:02:48 +0545

    change

---
+++ frontend/quasar.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  framework: {
+    config: {
+      // optional if you want to use
+      // Quasar's default settings
+    } 
+  },
+  plugins: [] // add Quasar plugins here if needed
+}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,20 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import './style.css';
+import './style.css'
+import { Quasar,QBtn,Notify, QInput, Dialog } from 'quasar'
 
-createApp(App).mount('#app')
+// Import icon libraries (optional)
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+createApp(App)
+  .use(Quasar, {
+    components: { QBtn, QInput },
+    plugins: {Notify, Dialog}, // import Quasar plugins and add here if needed
+    config: {
+      dark: true // force dark mode
+    }// optional if you want to use default config
+  })
+  .mount('#app')
